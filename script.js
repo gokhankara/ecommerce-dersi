5801e259f12a3c644a35d2f9ab8e84f5963c25c8
Ayhan Sipahi <info@ayhansipahi.net>
2021-05-01 13:21:12 +0200

    array operations

--- script.js
+++ script.js
@@ -1,76 +1,190 @@
-// function
-// function f1 (){}
+var arr1 = [1,2,3]
+for (var i= 0; i < arr1.length; i++ ){
+    arr1[i] = arr1[i]+1;
+}
+console.log(arr1);
 
-// variable
-// var1 = undefined
-// f2 = undefined
+console.log( [1,2,3].map(function(i,index){return i+1}) ) 
 
-// let const
-// let1
-// const1
+console.log( [1,2,3].map(i=>i+1)) 
 
-//--------
-f1();
-f2 && f2();
-console.log(var1);
-//console.log(const1);
+function f1 (){}
 
-var var1 = 1;
-function f1() {}
-var f2 = function () {
-  console.log(123);
-};
-let let1 = "";
-const const1 = "";
+var f2 = function(){}
 
-f2 && f2();
+var f2 = ()=>{}
 
-["", null, undefined, 0, {}, []];
+// array methods 
 
-// operators
+map // array elemanlarının üzerinde gezerek tek tek elemanlar üzerinde verilen fonksiyonu çalıştırıp return edilen değeri üztüne yazar.
+forEach // array elemanlarını tek tek işleme alır, 
+find // array içerisinde verilen fonksiyona true dönen ilk elemanı verir
+filter // array içerisinde verilen fonksiyona true dönen tüm elemanları verir
+reduce // array içerisinde iterate ederek son bir değer oluşturmaya yarar 
+concat //arrayleri birleştirir
+flat // arraylerin içerisindeki arraylarin üst arraye taşınmasını sağlar
+
+
+pop
+unshift
+shift
+push
+
+/* var arr2=[];
+
+var arr1 =  [1,2,3];
+
+arr1.forEach((i, index)=>arr2[index] = i*3)
+
+var musteriler = [{
+    isim: "ali",
+    soyisim: "kemal"
+},{
+    isim: "hasan",
+    soyisim: "can"
+}]
+
+var musteriAdSoyadlari = [];
+
+musteriler.forEach((m, i)=>musteriAdSoyadlari[i]=  m.isim + " " + m.soyisim)
+
+var musteriAdSoyadlari2 = musteriler.map((m)=> m.isim + " " + m.soyisim)
+ */
+
 /*
-< küçüktür
-<= küçük eşit
-> büyüktür
->= büyük eşit
-== eşittir
-=== dönüştürmesiz eşittir (with out type coersion)
-|| yada 
-&& ve
-! değili
--- eksikt
-++ arttır
--n n eksilt
-+n n arttır
-? ternary operator
+var arr =[
+    {
+        id: 234234,
+        gender: "male",
+        cari: 10000
+    },
+    {
+        id: 45645,
+        name:"ayhan",
+        gender: "male",
+        cari: 20000
+    },
+    {
+        id: 345345,
+        name:"aylin",
+        gender: "female",
+        cari: 30000
+    },
+
+]
+
+arr.filter(el=>{
+    return el.gender === "male"
+})
+arr.find(el=>{
+    return el.gender === "male"
+})
+
+arr.filter(el=>{
+    return el.gender === "male"
+}).map(el=>{return { ...el, status: true}})
+
+arr.find(el=>{
+    return el.id === 345345;
+})
+
+var arr ={
+    234234: {
+        id: 234234,
+        gender: "male"
+
+    },
+    45645: {
+        id: 45645,
+        name:"ayhan",
+        gender: "male"
+    },
+    345345: {
+        id: 345345,
+        name:"aylin",
+        gender: "female"
+    },
+}
+
+var musteriId = 345345;
+arr[musteriId];
+*/
+/*
+var arr =[
+    {
+        id: 234234,
+        gender: "male",
+        cari: 10000
+    },
+    {
+        id: 45645,
+        name:"ayhan",
+        gender: "male",
+        cari: 20000
+    },
+    {
+        id: 345345,
+        name:"aylin",
+        gender: "female",
+        cari: 30000
+    },
+    {
+        id: 345,
+        name:"aylin",
+        gender: "female",
+        cari: 100000
+    },
+]
+arr.reduce((onceki,sonraki) => {
+    return onceki + sonraki.cari
+}, 0);
+
+
+arr.reduce((onceki,sonraki)=>{
+    onceki[sonraki.id] = sonraki;
+     return onceki;
+}, {})
+
+
+arr.reduce((onceki,sonraki)=>{
+    if (sonraki.gender ==="female"){
+      onceki[sonraki.id] = sonraki;
+      onceki.total += sonraki.cari
+    }
+    return onceki;
+}, {total:0})
+
+// concat
+var arr1 = [1,2,3], arr2 = [5,6,7];
+arr1.concat(arr2);
+
+// flat
+var multiDarr = [["ali","hasan",["veli",["ahmet"]]],["ayşe", "fatma"]];
+
+multiDarr.flat();
+multiDarr.flat(Infinity)
 */
 
-/*function isFalse(deger1){
-    if(deger1 == false){
-        console.log("deger false dur");
-    } else {
-        console.log("deger false değildir");
-    }
-}
-*/
+var arr1 =  [1,2,3,4,5,6];
 
-//kuşul ? (true ise çalışacak kod) : (false ise çalışacak)
+arr1.pop(); //son elemanı alır
+arr1.shift(); //ilk elemanı alır
+arr1.push(); //sona eleman ekler
+arr1.unshift(6); //başa eleman ekler
 
-for (let i = 0; i < 10; i++) {
-  i > 5 || i == 0
-    ? console.log("5 ten büyük yada 0")
-    : i < 4
-    ? console.log("4 ten küçük")
-    : console.log("4ten büyük 5 ten küçük");
-}
-for (let i = 0; i < 10; i++) {
-  if (i > 5 || i == 0) {
-    console.log("5 ten büyük yada 0");
-  } else {
-    if (i < 4) {
-      console.log("4 ten küçük");
-    } else {
-      console.log("4ten büyük 5 ten küçük");
-    }
-  }
-}
+var arr1 =  [1,2,3,4,5,6];
+
+
+arr1.slice(0)
+//(6) [1, 2, 3, 4, 5, 6]
+arr2 = arr1.slice(0)
+//(6) [1, 2, 3, 4, 5, 6]
+arr2.pop()
+//6
+arr2
+//(5) [1, 2, 3, 4, 5]
+arr1
+//(6) [1, 2, 3, 4, 5, 6]
+
+
+var index = arr1.findIndex(4);
