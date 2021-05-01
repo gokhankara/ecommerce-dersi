c6162ef97fc9f23c2f57d1ea3c79f494145ca313
Ayhan Sipahi <info@ayhansipahi.net>
2021-05-01 15:24:56 +0200

    vue giriş

--- script.js
+++ script.js
@@ -1,62 +1,24 @@
-var arr1 = [1,2,3];
-var arr2 = [4,5,6];
-
-arr1= [...arr1, 5];
-(4) [1, 2, 3, 5]
-arr1= [...arr1];
-(4) [1, 2, 3, 5]
-arr1= [5, ...arr1];
-(5) [5, 1, 2, 3, 5]
-arr1= [...arr1.filter(e=>e!==2)];
-(4) [5, 1, 3, 5]
-
-const newArr = [...arr1, ...arr2];
-
-const func = (first ,second, ...rest)=>{
-      console.log(first, second, rest)  
-}
-func (1,2,3,4,5,6)
-// 1 2 [3, 4, 5, 6]
-
-
-var obj= {
-    key: "key",
-    func: function(){
-
-    }
-};
-var obj= {
-    key,
-    func(){
-
-    }
-};
-
-const [a,b] = [1,2];
-
-var user = {
-    ad: "ayhan",
-    soyad: "sipahi",
-    yas: 32
-};
-var user2 = {
-    ad: "ayhan",
-    soyad: "sipahi",
-};
-const {ad,yas} = user
-console.log(ad, yas)
-
-function adyaz({ad,yas}) {
-    //var yas = obj.yas;
-    //var ad = obj.ad;
-    console.log(ad, yas)
-}
-function adyaz2( {ad,yas=18} ) {
-    //var yas = obj.yas;
-    //var ad = obj.ad;
-    console.log(ad, yas)
-}
-
-function addToBasket({productId, count=1}){
-    
-}
+var app = new Vue({
+  el: "#app",
+  data: {
+    tarih: new Date().getTime(),
+    number: 6,
+    users: [{ ad: "ayhan" }, { ad: "ahmet" }, { ad: "mehmet" }],
+    isChecked: false,
+  },
+  methods: {
+    merhaba() {
+      console.log("merhaba");
+    },
+    onHover() {
+      console.log("merhabanın üzerine gelindi.");
+    },
+    handleChange({ target: { value } }) {
+      ///var regex = new RegExp(/[0-9]+/,"g")
+      //if (value.match(regex)){
+      console.log(value);
+      this.tarih = value;
+      //}
+    },
+  },
+});
