954e83a4f8d161f0a21e3ce21e31cd10764ce2c2
Ayhan Sipahi <info@ayhansipahi.net>
2021-05-01 10:07:21 +0200

    hoisting 1

--- script.js
+++ script.js
@@ -1,40 +1,17 @@
+// function
+// function f1 (){}
 
-var var1 = "global scope var";
-let let1 = "";
-const const1 ="";
+// variable
+// var1 = undefined
+// f2 = undefined
 
-// scopes
-// 1- global scope
-// 2- function scope
-// 3- block scope
+// let const
+// let1
+// const1
 
-/* 
-function func1(){
-    var varf = "function scope var"
-    varfg = "global scope varfg"
-}
-for (let i = 0; i<5;i++){
-    console.log({i});
-}
-console.log(i); */
-
-
-
-// const const1 ="";
-// const1 = 123;
-
-// const const2 = {
-//     key1 : "value1"
-// }
-// const2.key1 = "value2"
-
-// console.log(const2);
-
-/* const const3 = Object.freeze({
-         key1 : "value1"
-     })
-     const3 = {a:123}
-
-
-console.log(const3); */
-
+//--------
+var var1 = 1;
+function f1 (){}
+var f2 = function(){}
+let let1 ="";
+const const1 = ""
